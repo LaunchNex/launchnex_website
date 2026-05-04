ae0a832e5c5bd9a6ad054ef1d08a551baed959ef
PriyanshuAgarwal <agarwal.iet888@gmail.com>
2026-05-04 19:40:00 +0530

    Add sitemap & robots.js

---
+++ src/app/robots.js
@@ -0,0 +1,12 @@
+const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+
+export default function robots() {
+	return {
+		rules: {
+			userAgent: "*",
+			allow: "/",
+			disallow: ["/api/", "/private/"],
+		},
+		sitemap: `${baseUrl}/sitemap.xml`,
+	};
+}
